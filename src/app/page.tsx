819a1f76034fb2a8ce6093a43bf3a8a64e9b8289
warmdev17 <warmdevofficial@gmail.com>
2025-06-28 23:15:38 +0700

    change head data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,32 @@
+import Head from "next/head";
 import Blogs from "./components/Blogs";
 import Hero from "./components/Hero";
 import Newsletter from "./components/Newsletter";
 import Projects from "./components/Projects";
 
-
 export default function Home() {
   return (
-    <main>
-      <Hero />
-      <Projects />
-      <Blogs />
-      <Newsletter />
-    </main>
+    <>
+      <Head>
+        <meta
+          name="description"
+          content="Dự án tên lửa Kestrel - kiểm soát, phóng, và theo dõi hành trình."
+        />
+
+        {/* Open Graph */}
+        <meta property="og:title" content="Portfolio - Home" />
+        <meta property="og:description" content="A portfolio website" />
+        <meta property="og:image" content="https://warmdev.io.vn/logo.jpg" />
+        <meta property="og:url" content="https://warmdev.io.vn" />
+        <meta property="og:type" content="website" />
+      </Head>{" "}
+      <main>
+        <Hero />
+        <Projects />
+        <Blogs />
+        <Newsletter />
+      </main>
+    </>
   );
-} 
+}
+
